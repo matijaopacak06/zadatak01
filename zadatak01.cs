28e068379e933e33a6c5445e3dba7d0793d35d1c
matijaopacak06 <153612293+matijaopacak06@users.noreply.github.com>
2023-12-12 12:17:44 +0100

    Add files via upload

---
+++ zadatak01.cs
@@ -0,0 +1,17 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		int a=100;
+		int b=3;
+		int x,y;
+		x=a/b;
+		y=a%b;
+		Console.WriteLine("rezultat je: {0}" ,+x );
+		Console.WriteLine("ostatak djeljenja je: {0}",+y);
+		
+	}
+	
+}
